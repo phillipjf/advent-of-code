96e46d7ad0feb44af9825fa290c3f6639e34cd91
Phillip Ferentinos <phillipjf@users.noreply.github.com>
2024-12-02 08:45:46 -0600

    feat(2024/01.1): stubs

---
+++ 2024/01/solution.py
@@ -0,0 +1,43 @@
+import argparse
+import logging
+from typing import List, Tuple
+from dataclasses import dataclass, field
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s][%(name)s.%(funcName)s:%(lineno)d] %(levelname)s: %(message)s",
+)
+
+
+def main(part: int):
+    with open("input") as f:
+        inf: List[str] = f.read().splitlines()
+
+    result = inf
+
+
+    logging.info(f"Part {'One' if part == 1 else 'Two'} Answer: {result}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "part",
+        metavar="P",
+        type=int,
+        choices=[1, 2],
+        help="Problem part (1 or 2)",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Be verbose",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+    )
+    args = parser.parse_args()
+    if args.loglevel:
+        logging.getLogger().setLevel(args.loglevel)
+    main(args.part)

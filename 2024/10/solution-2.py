0db53d8d01a97c5b273e3d90e60f182477f1ba0d
Phillip Ferentinos <phillipjf@users.noreply.github.com>
2025-12-02 09:18:30 -0600

    feat(2024/10): pushing incomplete solution

--- 2024/10/solution-2.py
+++ 2024/10/solution-2.py
@@ -3,7 +3,6 @@ import logging
 from typing import List, Tuple
 from dataclasses import dataclass, field
 
-
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s][%(name)s.%(funcName)s:%(lineno)d] %(levelname)s: %(message)s",
@@ -14,10 +13,10 @@ def get_adjacent_coordinates(
     x: int, y: int, x_max: int, y_max: int
 ) -> List[Tuple[int, int]]:
     possible_coordinates = [
-        (-0, -1),
+        (0, -1),
+        (0, 1),
         (-1, 0),
         (1, 0),
-        (-0, 1),
     ]
     adjacent_coordinates = [
         pc
@@ -32,69 +31,47 @@ def is_valid_coordinate(x: int, y: int, x_max: int, y_max: int) -> bool:
     return (x >= 0 and x <= x_max) and (y >= 0 and y <= y_max)
 
 
-# Python program to search word in 2D grid in 8 directions
-# This function searches for the given word
-# in all 8 directions from the coordinate.
-def search_2d(word: str, grid: List[str], x: int, y: int):
-    y_max = len(grid)
-    x_max = len(grid[0])
+def find_match(inf, word, x, y, l_idx):
+    word_len = len(word)
+    y_max = len(inf) - 1
+    x_max = len(inf[0]) - 1
 
-    if grid[y][x] != word[0]:
+    if l_idx == word_len:
+        logging.debug("Reached end of word.")
+        return True
+
+    if not is_valid_coordinate(x, y, x_max, y_max):
+        logging.debug(f"Coordinate at ({x}, {y}) is invalid.")
         return False
 
-    len_word = len(word)
-
-    adjacent_coordinates = get_adjacent_coordinates(x, y, x_max, y_max)
-    for dir in adjacent_coordinates:
-        xx = x + dir[0]
-        yy = y + dir[1]
-        k = 1
-
-        while k < len_word:
-            if not is_valid_coordinate(xx, yy, x_max, y_max):
-                logging.debug(f"Coordinate at ({xx}, {yy}) is invalid.")
-                break
-            curr_val = grid[yy][xx]
-            logging.debug(f"Coordinate at ({xx}, {yy}) is '{curr_val}'.")
-            if curr_val != word[k]:
-                break
-
-            xx += dir[0]
-            yy += dir[1]
-
-            k += 1
-
-        if k == len_word:
-            return True
+    if inf[y][x] == word[l_idx]:
+        logging.debug(f"Found {word[l_idx]} at ({x}, {y})")
+        temp = inf[y][x]
+        inf[y][x] = "#"
+        ac = get_adjacent_coordinates(x, y, x_max, y_max)
+        res = any([find_match(inf, word, x + xx, y + yy, l_idx + 1) for xx, yy in ac])
+        inf[y][x] = temp
+        return res
 
     return False
 
 
-def search_word(word: str, grid: List[str]):
-    y_max = len(grid)
-    x_max = len(grid[0])
+def search_word(word: str, inf: List[List[str]]):
+    y_max = len(inf) - 1
+    x_max = len(inf[0]) - 1
 
-    ans = []
     for y in range(y_max):
         for x in range(x_max):
-            # if the word is found from this coordinate, then append it to result.
-            if search_2d(word, grid, x, y):
-                ans.append((x, y))
-    return ans
-
-
-def print_result(ans):
-    for coord in ans:
-        print(f"{{{coord[0]},{coord[1]}}}", end=" ")
-    print()
+            if find_match(inf, word, x, y, 0):
+                return True
+    return False
 
 
 def main(part: int):
     with open("input.3.sample") as f:
-        inf: List[str] = f.read().splitlines()
+        inf: List[List[str]] = [list(l) for l in f.read().splitlines()]
     word: str = "0123456789"
     result = search_word(word, inf)
-    print_result(result)
     logging.info(f"Part {'One' if part ==1 else 'Two'} Answer: {result}")
 
 
